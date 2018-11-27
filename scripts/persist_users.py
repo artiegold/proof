9a9d0b5f81a7927678e851db4e5f5ca8118f7c7a
Artie Gold <the.artiegold@gmail.com>
2018-11-26 23:55:32 -0600

    start scripts

---
+++ scripts/persist_users.py
@@ -0,0 +1,12 @@
+import db_interface
+import csv_interface
+
+csv = csv_interface.Extract_CSV_Info(
+    '../data/input/Proof_homework.csv',
+    ['User ID', 'IP', 'Geo', 'Industry', 'Company Size']
+)
+
+db = db_interface.db_interface('')
+
+print db.add_users(csv.read_input_file())
+
