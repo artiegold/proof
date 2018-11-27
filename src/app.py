2e1276d552917da90e3b5d6f2156b0dc4df53104
Artie Gold <the.artiegold@gmail.com>
2018-11-27 09:55:52 -0600

    fix template

--- src/app.py
+++ src/app.py
@@ -1,5 +1,14 @@
 from flask import Flask, abort, request, render_template
 import json
+import db_interface
+import get_image
+
+db = db_interface.db_interface('')
+im = get_image.get_image(db.get_rules(), db.get_campaigns())
+
+def get_basename(ip_address):
+    user = db.get_user(ip_address)
+    return im.get_image_basename(user)
 
 app = Flask(__name__)
 
@@ -9,11 +18,13 @@ def ping():
 
 @app.route('/', methods=['GET'])
 def main():
-    return render_template('template.html', basename="shrug.jpg")
+    ip_address = request.remote_addr
+    basename = get_basename(ip_address)
+    return render_template('template.html', basename=basename)
 
 @app.route('/testip/<ip_address>', methods=['GET'])
 def test_ip(ip_address):
-    basename = ip_address
+    basename = get_basename(ip_address)
     return render_template('template.html', basename=basename)
 
 if __name__ == '__main__':
